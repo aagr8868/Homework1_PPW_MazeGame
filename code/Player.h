341025af01f1ede6829bb4890bc54c70a096f6d2
Aaron <aagr8868@colorado.edu>
2018-09-14 15:05:45 -0600

    Edited Where coments should be in code
    - No Double comments
    - No variables commented
    - No Classes comented
    - All incomplete Funcitons comented

--- code/Player.h
+++ code/Player.h
@@ -16,7 +16,7 @@ struct Position {
 class Player {
 public:
 	// TODO: implement
-	Player(const std::string name, const bool is_human);  // constructor
+	// Player(const std::string name, const bool is_human);  // constructor
 	
 	// These are already implemented for you
 	std::string get_name() const {return name_; }  // inline member function
@@ -24,22 +24,22 @@ public:
 	Position get_position() const {return pos_; }  // inline member function
 	bool is_human() const {return is_human_; }  // inline member function
 
-//	// TODO: implement the following functions
-//	// You MUST implement the following functions
+	// TODO: implement the following functions
+	// You MUST implement the following functions
 //	void ChangePoints(const int x);  
-//
-//	// set a new position for this player
+
+	// set a new position for this player
 //	void SetPosition(Position pos);
-//
-//	// You may want to implement these functions as well
-//	// ToRelativePosition is a function we used to translate positions
-//	// into directions relative to the player (up, down, etc)
+
+	// You may want to implement these functions as well
+	// ToRelativePosition is a function we used to translate positions
+	// into directions relative to the player (up, down, etc)
 //	std::string ToRelativePosition(Position other);
-//
-//	// Convert this player to a string representation of their name and points
+
+	// Convert this player to a string representation of their name and points
 //	std::string Stringify();
-//
-//	// You may add other functions as needed/wanted
+
+	// You may add other functions as needed/wanted
 
 private:
 	std::string name_;
