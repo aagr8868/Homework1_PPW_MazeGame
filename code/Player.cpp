2ce82c0c2482fd63bd9257006d95b3e2dd67cc6a
Aaron <aagr8868@colorado.edu>
2018-09-17 19:39:08 -0600

    Edited in A couple of Functions

---
+++ code/Player.cpp
@@ -0,0 +1,33 @@
+#ifndef _PLAYER_CPP
+#define _PLAYER_CPP
+
+#include "Player.h"
+#include <cstdlib>
+#include <time>
+
+srand(time(NULL));
+
+Player::Player(const std::string name, const bool is_human)
+{
+	name_ = name;
+	points_ = 0;
+	pos_.row = 0;
+	pos_.col = 0;
+	is_human_ = is_human;
+	
+	if (!is_human)
+	{
+		do
+		{
+			pos_.row = rand() % 4 + 1;
+			pos_.col = rand() % 4 + 1;
+		}while (pos_.row == 0 && pos_.col == 0);
+	}
+}
+
+void Player::ChangePoints(const int x)
+{
+	
+}
+
+#endif //_PLAYER_CPP
