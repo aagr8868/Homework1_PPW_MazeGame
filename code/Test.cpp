21e85f28ae4d844d6ac5a8ac0e408de85e51ccc2
Aaron <aagr8868@colorado.edu>
2018-09-17 21:45:11 -0600

    Ran Tests

--- code/Test.cpp
+++ code/Test.cpp
@@ -5,24 +5,29 @@ using namespace std;
 
 int main ()
 {
-	Player P("Aaron", true);
+	Player P1("Aaron", false);
+	Player P2("Perrin", false);
+	Player P3("Chris", false);
+	Player P4("Dylan", true);
+
 	Position PP;
 
-	PP = P.get_position();
+	Player arr[4] = {P1, P2, P3, P4};
 
-	
-	cout << "Starting Name: " << P.get_name() << endl;
-	cout << "Starting Points: " << P.get_points() << endl;
-	cout << "Starting Position: " << PP.row << " , " << PP.col << endl;
-	cout << "Starting Humanity: ";
+	for (int i = 0; i < 3; i++)
+	{
+		PP = arr[i].get_position();
 
-	if (P.is_human())
-		cout << "TRUE";
-	else
-		cout << "False";
-	cout << endl;
+		cout << "Starting Name: " << arr[i].get_name() << endl;
+		cout << "Starting Points: " << arr[i].get_points() << endl;
+		cout << "Starting Position:" << PP.row << "," << PP.col << endl;
+		cout << "Starting Humanity: ";
 
-	
-
+		if (arr[i].is_human())
+			cout << "TRUE";
+		else
+			cout << "FALSE";
+		cout << endl;
+	}
 	return 0;
 }
